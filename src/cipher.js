ec6e8d4420f6577f1a9a09cb3c5dd734e15267fa
KarineVerneque <karinemvs01@hotmail.com>
2019-08-19 17:58:55 -0300

    adicionando a estrutura html

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,19 @@
 window.cipher = {
-  // ... 
+  encode: encode,
+  decode: decode
+  
+  
+  // ... criar a função de cifrar e decifrar
 };
+
+function encode(ossfet, string){
+
+  console.log()
+  //magica
+}
+
+
+
+function decode(){
+  //magica
+}
---
+++ src/index.js
@@ -0,0 +1,21 @@
+function cifra(){
+    let firstMsg = document.getElementById('firstMsg').value;
+    let number = document.getElementById('deslocamento').value;
+  
+    /*
+    if (firstMsg == 'mensagem'){
+        document.getElementById('write1').innerHTML = 'Mensagem recebida'
+  
+    } else {
+      document.getElementById('write2').innerHTML = 'Mensagem NÃO reconhecida'
+    }
+    */
+  
+  
+  }
+
+
+
+//function clickaqui(){
+//  alert('chamou');
+//}
